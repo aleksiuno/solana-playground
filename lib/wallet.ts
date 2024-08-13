c34980bee105680ce200313d74dce88ff77674e9
Darius Aleksiunas <aleksiuno@gmail.com>
2024-08-14 01:10:45 +0300

    add solana explorer urls

--- lib/wallet.ts
+++ lib/wallet.ts
@@ -9,21 +9,28 @@ import {
 export const generateKeypair = async (): Promise<void> => {
   const keypair = Keypair.generate()
   await addKeypairToEnvFile(keypair, 'SECRET_KEY', '.env')
+  const publicKey = keypair.publicKey.toBase58()
 
-  console.log(`Public key: `, keypair.publicKey.toBase58())
+  console.log(`Public key: `, publicKey)
   console.log(`Private key: `, keypair.secretKey)
   console.log(`Private key encoded: `, bs58.encode(keypair.secretKey))
   console.log(`✅ Finished!`)
+  console.log(
+    `You can find more details about wallet here 👉 https://explorer.solana.com/address/${publicKey}?cluster=devnet`
+  )
 }
 
 export const getKeypairFromEnv = (): Keypair => {
   const secret = Uint8Array.from(JSON.parse(process.env.SECRET_KEY!))
   const keypair = Keypair.fromSecretKey(secret)
+  const publicKey = keypair.publicKey.toBase58()
   // @TODO seems like this helper function doesn't work correctly on current version of solana-developers/helpers
   // const keypair = getKeypairFromEnvironment("SECRET_KEY");
-  console.log(`Public key: ${keypair.publicKey.toBase58()}`)
+  console.log(`Public key: ${publicKey}`)
   console.log(`✅ Finished! We've loaded our secret key securely, using an env file!`)
-
+  console.log(
+    `You can find more details about wallet here 👉 https://explorer.solana.com/address/${publicKey}?cluster=devnet`
+  )
   return keypair
 }
 
@@ -42,6 +49,9 @@ export const getBalance = async (walletAddress: string): Promise<void> => {
   const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL
 
   console.log(`✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`)
+  console.log(
+    `You can find more details about wallet here 👉 https://explorer.solana.com/address/${publicKey}?cluster=devnet`
+  )
 }
 
 export const transfer = async (walletAddress: string, amount: number): Promise<void> => {
@@ -51,9 +61,6 @@ export const transfer = async (walletAddress: string, amount: number): Promise<v
   }
 
   const senderKeypair = getKeypairFromEnv()
-
-  console.log(`suppliedToPubkey: ${walletAddress}`)
-
   const toPubkey = new PublicKey(walletAddress)
   const connection = new Connection('https://api.devnet.solana.com', 'confirmed')
 
@@ -71,6 +78,9 @@ export const transfer = async (walletAddress: string, amount: number): Promise<v
 
   const signature = await sendAndConfirmTransaction(connection, transaction, [senderKeypair])
 
-  console.log(`💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `)
+  console.log(`💸 Finished! Sent ${amount}SOL to the address ${toPubkey}. `)
   console.log(`Transaction signature is ${signature}!`)
+  console.log(
+    `You can find more details about transaction here 👉 https://explorer.solana.com/tx/${signature}?cluster=devnet`
+  )
 }
